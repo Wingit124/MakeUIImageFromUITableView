417cdc98eb562a01858808258259099a321771c0
Takahashi Tsubasa <win9it.124@gmail.com>
2021-03-25 23:32:51 +0900

    tableViewとsuperviewのサイズが同じ場合は綺麗にキャプチャできるようになった サイズが違う場合にも撮れるようにしたい

--- MakeUIImageFromUITableView/Extension/UITableView+Extension.swift
+++ MakeUIImageFromUITableView/Extension/UITableView+Extension.swift
@@ -15,41 +15,45 @@ extension UITableView {
         }
     }
     
+    private var contentBottom: CGFloat {
+        return contentSize.height - bounds.height
+    }
+    
     private func generateContentImage() -> UIImage? {
         
-        let originalOffsetY = self.contentOffset.y
-        self.isScrollEnabled = false
-        self.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
-        
-        var contentHeight = self.contentSize.height
-        let visibleHeight = self.visibleSize.height
+        let originalOffsetY = contentOffset.y
+        isScrollEnabled = false
+        scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: false)
         
         var captureImages: [UIImage] = []
         //コンテンツの高さ分スクロールを繰り返してキャプチャする
-        while (contentHeight > 0) {
-            if let image = superview?.image {
-                captureImages.append(image)
+        while true {
+            
+            if let image = superview?.image { captureImages.append(image) }
+            
+            if contentOffset.y < (contentBottom - bounds.height) {
+                contentOffset.y += bounds.height
+            } else {
+                contentOffset.y = contentBottom
+                if let image = superview?.image { captureImages.append(image) }
+                break
             }
-            //現在表示されてる高さを全体から引く
-            contentHeight -= self.visibleSize.height
-            self.contentOffset.y += self.visibleSize.height
         }
         //画像を結合
         UIGraphicsBeginImageContext(contentSize)
         var y: CGFloat = 0
         for image in captureImages {
-            print(y)
             image.draw(at: CGPoint(x: 0, y: y))
-            y += visibleHeight
+            y = min(y + bounds.height, contentBottom)
         }
         let mergedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         
-        self.isScrollEnabled = true
-        self.contentOffset.y = originalOffsetY
+        isScrollEnabled = true
+        contentOffset.y = originalOffsetY
         
         return mergedImage
     }
-
+    
     
 }
