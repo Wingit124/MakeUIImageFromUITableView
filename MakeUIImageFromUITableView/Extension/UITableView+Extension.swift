980d7da4ba769a0d33324d46ac148aa269657fe3
Takahashi Tsubasa <win9it.124@gmail.com>
2021-03-28 21:49:36 +0900

    軽微な調整

--- MakeUIImageFromUITableView/Extension/UITableView+Extension.swift
+++ MakeUIImageFromUITableView/Extension/UITableView+Extension.swift
@@ -8,36 +8,45 @@
 import UIKit
 
 extension UITableView {
+    /// テーブルをキャプチャする前の状態を保持するストラクト
+    private struct OriginalState {
+        let contentOffset: CGPoint
+        let isUserInteractionEnabled: Bool
+        let showsVerticalScrollIndicator: Bool
+    }
     /// 全コンテンツのキャプチャ画像
     var contentImage: UIImage? {
         get {
             return generateContentImage()
         }
     }
-    
     /// テーブルの全コンテンツのキャプチャ画像を生成する処理
     /// - Returns: 全コンテンツのキャプチャ画像
     private func generateContentImage() -> UIImage? {
-        //テーブルの状態を変更
-        let originalOffsetY = contentOffset.y
-        isScrollEnabled = false
+        //テーブルの状態を保存
+        let originalState = OriginalState(contentOffset: contentOffset,
+                                          isUserInteractionEnabled: isUserInteractionEnabled,
+                                          showsVerticalScrollIndicator: showsVerticalScrollIndicator)
+        //スクリーンショット用に状態を変更
         contentOffset.y = 0
+        isUserInteractionEnabled = false
+        showsVerticalScrollIndicator = false
         //キャプチャされた画像が入ってくる
         var captureImages: [UIImage] = []
         //コンテンツの高さ分スクロールを繰り返してキャプチャする
-        var currentHeight: CGFloat = contentSize.height
-        while currentHeight > 0 {
+        var currentY: CGFloat = contentSize.height
+        while currentY > 0 {
             //親ビューとの差を計算
             let topDiff = frame.minY - (superview?.frame.minY ?? 0)
             let leadingDiff = frame.minX - (superview?.frame.minX ?? 0)
-            //トリミング後のサイズ
+            //トリミングに使うサイズ
             let cllipingRect = CGRect(x: leadingDiff, y: topDiff, width: visibleSize.width, height: visibleSize.height)
-            //キャプチャ後にトリミングして配列に詰める
+            //親ビューをキャプチャ後にトリミングして配列に詰める
             if let image = superview?.image?.clipping(to: cllipingRect) {
                 captureImages.append(image)
             }
             //スクロールする
-            currentHeight -= visibleSize.height
+            currentY -= visibleSize.height
             contentOffset.y += visibleSize.height
         }
         //画像を結合
@@ -50,8 +59,9 @@ extension UITableView {
         let mergedImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
         //テーブルの状態を元に戻す
-        isScrollEnabled = true
-        contentOffset.y = originalOffsetY
+        contentOffset = originalState.contentOffset
+        isUserInteractionEnabled = originalState.isUserInteractionEnabled
+        showsVerticalScrollIndicator = originalState.showsVerticalScrollIndicator
         //画像を返す
         return mergedImage
     }
